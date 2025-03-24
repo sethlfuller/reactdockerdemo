2af6d02f364e7d5a5aaa7a9baaad9c7422f2bb7a
Seth Fuller <seth.fuller@umbrage.com>
2025-03-24 12:55:14 -0500

    fix docker image

--- myApp/frontend/entrypoint.sh
+++ myApp/frontend/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -e
 
+# Add debug line to entrypoint.sh
+echo "Entrypoint script location: $(pwd)/entrypoint.sh"
+ls -l /app/entrypoint.sh
+
 # Start SSH Server
 /usr/sbin/sshd -D &
 
 # Start Node.js Application
-exec npm start
+exec npm start -- --host 0.0.0.0
